3949b510495b1c2e01cabc125238d0c07d16caab
Sam Noh <samnoh21@outlook.com>
2019-11-26 21:03:43 +1300

    feat(graphql): update resolver

--- src/graphql/root.js
+++ src/graphql/root.js
@@ -1,8 +1,58 @@
+import db from 'models';
+
+const { User, Post } = db;
+
+const Query = {
+    user: async ({ id }) => {
+        const user = await User.findByPk(id);
+        return user;
+    },
+    users: async () => {
+        const users = await User.findAll({});
+        return users;
+    },
+    post: async ({ id }) => {
+        const posts = await Post.findAll({
+            where: { userId: id },
+            order: [['createdAt', 'DESC']]
+        });
+
+        return posts;
+    },
+    posts: async () => {
+        const posts = await Post.findAll({
+            order: [['createdAt', 'DESC']]
+        });
+        return posts;
+    }
+};
+
+const Mutation = {
+    signUp: async ({ username, password }) => {
+        const user = await User.findOne({ where: { username } });
+        if (user) {
+            return false;
+        }
+        await User.create({ username, password });
+        return true;
+    },
+    addPost: async ({ userId, title, content }) => {
+        const user = await User.findByPk(userId);
+        if (!user) {
+            return false;
+        }
+        await Post.create({
+            title,
+            content,
+            userId: user.id
+        });
+        return true;
+    }
+};
+
 const root = {
-    hello: () => 'Hello world!',
-    quoteOfTheDay: () => (Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within'),
-    random: () => Math.random(),
-    rollThreeDice: () => [...Array(3)].map(() => 1 + Math.floor(Math.random() * 6))
+    ...Query,
+    ...Mutation
 };
 
 export default root;
