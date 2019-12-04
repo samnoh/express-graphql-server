dc28cf131d38f5ea504c45db1f97ab88e16261b9
Sam Noh <samnoh21@outlook.com>
2019-12-04 14:26:21 +1300

    feat(app_post): update editor

--- app/src/components/post/Editor.js
+++ app/src/components/post/Editor.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState, useCallback } from 'react';
+import React, { useEffect, useRef, useCallback } from 'react';
 import Quill from 'quill';
 import 'quill/dist/quill.bubble.css';
 import styled from 'styled-components';
@@ -6,13 +6,19 @@ import styled from 'styled-components';
 const Container = styled.section``;
 
 const TitleInput = styled.input`
-    font-size: 42px;
+    font-size: 38px;
+    color: #2c3e50;
     outline: none;
     padding-bottom: 10px;
     border: none;
-    border-bottom: 1px solid #eaecef;
-    margin-bottom: 40px;
+    border-top: 1px solid #eaecef;
+    border-left: 1px solid #eaecef;
+    border-right: 1px solid #eaecef;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+    padding: 10px 15px;
     width: 100%;
+
     &::placeholder {
         color: #eaecef;
     }
@@ -20,14 +26,18 @@ const TitleInput = styled.input`
 
 const QuillWrapper = styled.div`
     .ql-editor {
-        padding: 0;
-        min-height: 320px;
+        color: #2c3e50;
+        min-height: calc(100vh - 320px);
         font-size: 17px;
         line-height: 1.5;
+        border: 1px solid #eaecef;
+        border-bottom-left-radius: 10px;
+        border-bottom-right-radius: 10px;
+        padding: 15px;
     }
     .ql-editor.ql-blank::before {
         left: 0;
-        z-index: 1;
+        z-index: 999;
 
         &::placeholder {
             color: #eaecef;
@@ -36,15 +46,15 @@ const QuillWrapper = styled.div`
 `;
 
 const Editor = ({ title = '', body = '' }) => {
-    const [values, setValues] = useState({ title, body });
+    const values = useRef({ title, body });
     const quillElement = useRef(null);
     const quillInstance = useRef(null);
 
     const onChange = useCallback(
         e => {
-            setValues({ ...values, [e.target.name]: e.target.value });
+            values.current = { ...values.current, [e.target.name]: e.target.value };
         },
-        [values, setValues]
+        [values]
     );
 
     useEffect(() => {
@@ -55,7 +65,7 @@ const Editor = ({ title = '', body = '' }) => {
                     [{ header: '1' }, { header: '2' }],
                     ['bold', 'italic', 'underline', 'strike'],
                     [{ list: 'ordered' }, { list: 'bullet' }],
-                    ['blockquote', 'code-block', 'link', 'image']
+                    ['blockquote', 'code-block', 'link']
                 ]
             }
         });
@@ -70,7 +80,7 @@ const Editor = ({ title = '', body = '' }) => {
 
     return (
         <Container>
-            <TitleInput placeholder="Title" onChange={onChange} name="title" value={values.title} />
+            <TitleInput placeholder="Title" onChange={onChange} name="title" autoFocus />
             <QuillWrapper>
                 <div ref={quillElement} />
             </QuillWrapper>
