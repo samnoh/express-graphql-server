e3120130b3cbdb5c6def158dbbb9f2a6a8641800
Sam Noh <samnoh21@outlook.com>
2020-01-06 21:59:59 +1300

    chore(): clean up

--- app/src/App.js
+++ app/src/App.js
@@ -2,6 +2,7 @@ import React, { lazy, Suspense } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import { GlobalStyle } from 'styles';
+import ErrorPage from 'pages/ErrorPage';
 
 const PostsPage = lazy(() => import('pages/PostsPage'));
 const PostPage = lazy(() => import('pages/PostPage'));
@@ -18,7 +19,7 @@ const App = () => {
     return (
         <>
             <GlobalStyle />
-            <Suspense fallback={null}>
+            <Suspense fallback={ErrorPage}>
                 <Switch>
                     <Route exact path="/" component={PostsPage} />
                     <Route path="/post/new" component={EditorPage} />
--- app/src/index.js
+++ app/src/index.js
@@ -8,7 +8,6 @@ import store from 'store';
 import App from 'App';
 import client from 'graphql/client';
 import { setToken } from 'store/actions/auth';
-
 import * as serviceWorker from 'serviceWorker';
 
 const checkLogin = () => {
@@ -38,4 +37,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
