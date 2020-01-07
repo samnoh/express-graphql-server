94f4cb7c969518d6b2a558f49d88b4fe16bc98ba
Sam Noh <samnoh21@outlook.com>
2020-01-07 20:39:59 +1300

    refactor(app_utils): getTime

--- app/src/utils/index.js
+++ app/src/utils/index.js
@@ -1,5 +1,3 @@
-const now = new Date().getTime();
-
 export const capitalize = text => {
     const [firstLetter, ...rest] = text;
     return firstLetter.toUpperCase() + rest.join('');
@@ -11,24 +9,26 @@ export const plurize = (number, text, option) => {
     return parseInt(number) === 1 ? text + (single ? single : '') : text + (plural ? plural : 's');
 };
 
-export const getTime = time => {
-    const _time = parseInt(time);
-    const date = new Date(_time);
-    const diff = now - _time;
+const times = [
+    { ms: 1000, unit: 'milliseconds' },
+    { ms: 60000, unit: 'second' },
+    { ms: 3600000, unit: 'minute' },
+    { ms: 86400000, unit: 'hour' },
+    { ms: 172800000, unit: 'yesterday' },
+    { ms: 259200000, unit: 'two days ago' }
+];
 
-    if (diff <= 60000) {
-        // less than a second
-        let seconds = parseInt(diff / 1000);
-        if (seconds < 0) seconds = 0;
-        return `${seconds} ${plurize(seconds, ' second')} ago`;
-    } else if (diff <= 3600000) {
-        // less than a minute
-        const minutes = parseInt(diff / 60000);
-        return `${minutes} ${plurize(minutes, ' minute')} ago`;
-    } else if (diff <= 86400000) {
-        // less than a hour
-        const hours = parseInt(diff / 3600000);
-        return `${hours} ${plurize(hours, ' hour')} ago`;
+export const getTime = time => {
+    const _time = parseInt(time),
+        diff = new Date().getTime() - _time;
+
+    for (let i = 1; i < times.length; i++) {
+        if (diff < times[i].ms) {
+            let currTime = parseInt(diff / times[i - 1].ms);
+            if (currTime < 0) currTime = 0;
+            const unit = times[i].unit;
+            return diff < times[3].ms ? `${currTime} ${plurize(currTime, unit)} ago` : unit;
+        }
     }
-    return date.toLocaleDateString('en');
+    return new Date(_time).toLocaleDateString('en');
 };
